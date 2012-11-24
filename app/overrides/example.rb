6c2c6c6e2809c10f092935c85754a1074a6f9489
Vladislav Petrov <electronicchest@gmail.com>
2012-11-25 00:08:28 +0400

    second

--- app/overrides/example.rb
+++ app/overrides/example.rb
@@ -1,5 +1,5 @@
 Deface::Override.new(:virtual_path => "spree/shared/_main_nav_bar", 
                      :name => "example", 
                      :replace => "li#home-link") do
-                     "<li id=\"home-link\" data-hook><%= link_to t(:homer), root_path %></li>"
+                     "<li id=\"home-link\" data-hook><%= link_to t(:home), root_path %></li>"
                    end
